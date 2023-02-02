e6fc6f8f59fdf530b9a33b193537269f7cf8d878
tmsheak <tmsheak@gmail.com>
2023-02-02 13:14:53 -0500

    Corrected Logout Method

--- src/test/java/com/tarekacademy/LoginPages/LoginPage.java
+++ src/test/java/com/tarekacademy/LoginPages/LoginPage.java
@@ -6,50 +6,57 @@ import com.tarekacademy.base.BasePage;
 
 //The various elements on the page are defined as variables in this class
 public class LoginPage extends BasePage {
-	//once you extended BasePage, constructor need to created explicitly
+	// once you extended BasePage, constructor need to created explicitly
 
 	public LoginPage(WebDriver driver) {
 		super(driver); // means using Parent's driver
-		
-	}
-	
-	//By Class:	Initializes a new instance of the By class using the given functions to find element 
-	  By userName=By.id("ap_email");
-  public void setUsername(String input) {
-	// This need to call from LoginTest  
-	  getSendKey(userName, input); 
-	  
-  }
 
-  By password=By.id("ap_password");
-  public void setPassword(String input) {
-	  
-  	  getSendKey(password, input); 	  
+	}
+
+	// By Class: Initializes a new instance of the By class using the given
+	// functions to find element
+	By userName = By.id("ap_email");
+
+	public void setUsername(String input) {
+		// This need to call from LoginTest
+		getSendKey(userName, input);
+
+	}
+
+	By password = By.id("ap_password");
+
+	public void setPassword(String input) {
+
+		getSendKey(password, input);
+	}
+
+	By clickOnSignIn = By.id("nav-link-accountList-nav-line-1");
+
+	public void setSignInClick() {
+		getSignInClick(clickOnSignIn);
+	}
+
+	By clickContinue = By.xpath("//input[@id='continue']");
+
+	public void setContiueButton() {
+		getClickOnContinue(clickContinue);
+
+	}
+
+	By clickSubmit = By.xpath("//input[@id='signInSubmit']");
+
+	public void setSubmitButton() {
+		getClickSignInSubmit(clickSubmit);
+
+	}
+
+	/*
+	 * By accountList = By.id("nav-link-accountList");
+	 * 
+	 * public void setMouseOverOnLIst() { getMouseHoverOnList(accountList); }
+	 * 
+	 * By logoutBTN = By.xpath("(//span[@class='nav-text'])[21]");
+	 * 
+	 * public void setLogout() { getLogoutButton(logoutBTN); }
+	 */
 }
- 
-  By clickOnSignIn=By.id("nav-link-accountList-nav-line-1");
-  public void setSignInClick() {	  
-	  getSignInClick(clickOnSignIn); 	  
-}
-  By clickContinue=By.xpath("//input[@id='continue']");
-  public void setContiueButton() {	 
-	  getClickOnContinue(clickContinue);
-		  
-}
-  By clickSubmit=By.xpath("//input[@id='signInSubmit']");
-  public void setSubmitButton() {	
-	  getClickSignInSubmit(clickSubmit);
-		  
-}
- // By clickLogout=By.xpath("(//span[@class='nav-text'])[21]\")));
-  By clickLogout=By.xpath("*[@id=\"nav-item-signout\"]/span");   
-  public void setLogout() {	
-	  getLogoutButton(clickLogout); //need to work
-  }
-  
-  By accountList=By.xpath("//*[@id=\"nav-link-accountList\"]");
-  public void setMouseOverOnLIst() {
-	 
-	  getMouseHoverOnList(accountList);
-  }
-} 
--- src/test/java/com/tarekacademy/LoginTests/LoginTest.java
+++ src/test/java/com/tarekacademy/LoginTests/LoginTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
 import com.tarekacademy.LoginPages.LoginPage;
 import com.tarekacademy.base.BaseTest;
 import io.cucumber.java.en.Given;
@@ -13,11 +12,11 @@ import io.cucumber.java.en.When;
 
 //BaseTest has ChromeDriver opening App
 public class LoginTest extends BaseTest {
-	//Is-A relationship with BaseTest ( using Extends keyword)
-	
+	// Is-A relationship with BaseTest ( using Extends keyword)
+
 	LoginPage lp;
-	//Has-A relationship with LoginPage
-	
+	// Has-A relationship with LoginPage
+
 	@Given("User is on Amazon Homepage")
 	public void user_is_on_amazon_homepage() {
 		chrome();
@@ -32,8 +31,8 @@ public class LoginTest extends BaseTest {
 
 	@When("User input valid username")
 	public void user_input_valid_username() {
-		lp.setUsername("Tarek"); // need valid username for continue
-	
+		lp.setUsername("tarsm@gmail.com"); // need valid username for continue
+
 	}
 
 	@When("User click on Continuebutton")
@@ -43,8 +42,8 @@ public class LoginTest extends BaseTest {
 
 	@When("User input valid password")
 	public void user_input_valid_password() {
-		lp.setPassword("Tarek123"); // need password 
-		
+		lp.setPassword("Meah@2023"); // need password
+
 	}
 
 	@When("User click on loginbutton")
@@ -53,42 +52,50 @@ public class LoginTest extends BaseTest {
 
 	}
 
+	@Then("User validate the landing page")
+	public void user_validate_the_landing_page() {
+		String actualUrl = driver.getCurrentUrl();
+		String expectedUrl = "https://www.amazon.com/gp/yourstore/home?path=%2Fgp%2Fyourstore%2Fhome&signIn=1&useRedirectOnSuccess=1&action=sign-out&ref_=nav_AccountFlyout_signout";
 	
-	  @Then("User validate the landing page") 
-	  public void user_validate_the_landing_page() {
-	  String actualUrl=driver.getCurrentUrl();
-	  String expectedUrl="https://www.amazon.com/gp/yourstore/home?path=%2Fgp%2Fyourstore%2Fhome&signIn=1&useRedirectOnSuccess=1&action=sign-out&ref_=nav_AccountFlyout_signout";
-	 // String expectedUrl="https://www.amazon.com/?ref_=nav_ya_signin";
-	  Assert.assertEquals(actualUrl, expectedUrl,"Url did not match");
-	  
-	  
-	  }
-	  
-	  @Then("User validate the page tittle")
-	  public void user_validate_the_page_tittle() {
-	  String actualTitle=driver.getTitle();
-	  String expectedTitle=" ";
-	  Assert.assertEquals(actualTitle, expectedTitle,"Title did not match");
-	  
-	  //closeApplication();
-	  
-	  }
-	  
-	 
-	@Then("User click on LogoutButton")
-	public void user_click_on_logoutButton() {
-		WebElement accList=driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]"));
-		Actions action=new Actions(driver);
-		action.moveToElement(accList).click().perform();
-		//lp.setMouseOverOnLIst();
+		Assert.assertEquals(actualUrl, expectedUrl, "Url did not match");
 
-		//closeApplication();
 	}
+
+	@Then("User validate the page tittle")
+	public void user_validate_the_page_tittle() {
+		String actualTitle = driver.getTitle();
+		String expectedTitle = " ";
+		Assert.assertEquals(actualTitle, expectedTitle, "Title did not match");
+
+		// closeApplication();
+
+	}
+
+	@Then("User click on LogoutButton")
+	public void user_click_on_logoutButton() throws InterruptedException {		
+
+			Actions a = new Actions(driver);
+			
+			WebElement accountList = driver.findElement(By.id("nav-link-accountList"));
+
+			WebElement logoutBTN = driver.findElement(By.xpath("(//span[@class='nav-text'])[21]"));
+
+			//mouse hover on account list, mouse hover on logout then click
+			//Logout from Account
+			a.moveToElement(accountList).moveToElement(logoutBTN).click().perform();
+			
+
+		}
+	
+}
+
+	// closeApplication();
+
 	/*
 	 * Assertion: String expectedURL= " given here"; STring
 	 * actualURL=driver.getCurrentUrl(); System.out.println(actualURL);
 	 * Assert.assertEquals("Does not match", expectedURL, actualURL);
 	 * 
-	  */
+	 */
 
-}
+
